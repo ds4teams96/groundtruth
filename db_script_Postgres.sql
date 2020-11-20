1d9144da803adc5bc8fa50a865d1005d379d9aab
ds4teams96 <74432331+ds4teams96@users.noreply.github.com>
2020-11-19 19:19:29 -0500

    Adding transaction log to database script
    Adding transaction log to database script

--- db_script_Postgres.sql
+++ db_script_Postgres.sql
@@ -1,3 +1,77 @@
+CREATE SCHEMA public AUTHORIZATION postgres;
+
+CREATE TYPE _BK2_mt_sentencia (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 8,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = BK2_mt_sentencia,
+	DELIMITER = ',');
+
+CREATE TYPE _BK_mt_sentencia (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 8,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = BK_mt_sentencia,
+	DELIMITER = ',');
+
+CREATE TYPE _BK_mt_sentencia_region (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 8,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = BK_mt_sentencia_region,
+	DELIMITER = ',');
+
+CREATE TYPE _BK_tt_antecedente (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 8,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = BK_tt_antecedente,
+	DELIMITER = ',');
+
+CREATE TYPE _BK_tt_encabezado (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 8,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = BK_tt_encabezado,
+	DELIMITER = ',');
+
+CREATE TYPE _BK_tt_radicado (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 8,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = BK_tt_radicado,
+	DELIMITER = ',');
+
 CREATE TYPE _mt_dane_censo_2018 (
 	INPUT = array_in,
 	OUTPUT = array_out,
@@ -44,6 +118,42 @@ CREATE TYPE _mt_sentencia (
 	STORAGE = any,
 	CATEGORY = A,
 	ELEMENT = mt_sentencia,
+	DELIMITER = ',');
+
+CREATE TYPE _mt_sentencia_rest (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 8,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = mt_sentencia_rest,
+	DELIMITER = ',');
+
+CREATE TYPE _temp_new_resuelve (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 8,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = temp_new_resuelve,
+	DELIMITER = ',');
+
+CREATE TYPE _temp_test_resuelve (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 8,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = temp_test_resuelve,
 	DELIMITER = ',');
 
 CREATE TYPE _tt_area (
@@ -142,6 +252,18 @@ CREATE TYPE _tt_localizacion (
 	ELEMENT = tt_localizacion,
 	DELIMITER = ',');
 
+CREATE TYPE _tt_log_transaccion (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 8,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = tt_log_transaccion,
+	DELIMITER = ',');
+
 CREATE TYPE _tt_matricula_inmobiliaria (
 	INPUT = array_in,
 	OUTPUT = array_out,
@@ -214,6 +336,18 @@ CREATE TYPE _tt_radicado (
 	ELEMENT = tt_radicado,
 	DELIMITER = ',');
 
+CREATE TYPE _tt_radicado_rest (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 8,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = tt_radicado_rest,
+	DELIMITER = ',');
+
 CREATE TYPE _tt_resuelve (
 	INPUT = array_in,
 	OUTPUT = array_out,
@@ -224,18 +358,6 @@ CREATE TYPE _tt_resuelve (
 	STORAGE = any,
 	CATEGORY = A,
 	ELEMENT = tt_resuelve,
-	DELIMITER = ',');
-
-CREATE TYPE _tt_resuelve_rest (
-	INPUT = array_in,
-	OUTPUT = array_out,
-	RECEIVE = array_recv,
-	SEND = array_send,
-	ANALYZE = array_typanalyze,
-	ALIGNMENT = 8,
-	STORAGE = any,
-	CATEGORY = A,
-	ELEMENT = tt_resuelve_rest,
 	DELIMITER = ',');
 
 CREATE TYPE _tt_resuelve_text (
@@ -274,6 +396,18 @@ CREATE TYPE _tt_solicitante (
 	ELEMENT = tt_solicitante,
 	DELIMITER = ',');
 
+CREATE TYPE _vw_certificado_radicacion_finalizado (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 8,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = vw_certificado_radicacion_finalizado,
+	DELIMITER = ',');
+
 CREATE TYPE _vw_pendiente_modelo (
 	INPUT = array_in,
 	OUTPUT = array_out,
@@ -296,6 +430,18 @@ CREATE TYPE _vw_rol_beneficiario (
 	STORAGE = any,
 	CATEGORY = A,
 	ELEMENT = vw_rol_beneficiario,
+	DELIMITER = ',');
+
+CREATE TYPE _vw_sentencia (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 8,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = vw_sentencia,
 	DELIMITER = ',');
 
 CREATE TYPE _vw_tipo_sentencia_grupo (
@@ -322,13 +468,7 @@ CREATE TYPE _vw_tipo_zona (
 	ELEMENT = vw_tipo_zona,
 	DELIMITER = ',');
 
-CREATE TABLE public."BK_tt_radicado" (
-	radicado text NULL,
-	principal text NULL,
-	"key" text NULL
-);
-
-CREATE TABLE public.mt_dane_censo_2018 (
+CREATE TABLE mt_dane_censo_2018 (
 	codane text NULL,
 	departamento text NULL,
 	municipio text NULL,
@@ -361,10 +501,11 @@ CREATE TABLE public.mt_dane_censo_2018 (
 	mujer_estrato_5 int4 NULL,
 	mujer_estrato_6 int4 NULL,
 	mujer_no_sabe int4 NULL,
-	mujer_not_applicable int4 NULL
+	mujer_not_applicable int4 NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.mt_dane_estrato (
+CREATE TABLE mt_dane_estrato (
 	codane text NULL,
 	departamento text NULL,
 	municipio text NULL,
@@ -376,10 +517,11 @@ CREATE TABLE public.mt_dane_estrato (
 	estrato_4 int4 NULL,
 	estrato_5 int4 NULL,
 	estrato_6 int4 NULL,
-	no_sabe int4 NULL
+	no_sabe int4 NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.mt_dane_geo (
+CREATE TABLE mt_dane_geo (
 	codigo_dep text NULL,
 	codigo_mun text NULL,
 	codigo_cp text NULL,
@@ -391,73 +533,82 @@ CREATE TABLE public.mt_dane_geo (
 	latitud float8 NULL,
 	nombre_distrito text NULL,
 	municipio_anm text NULL,
-	nombre_area_metrop text NULL
+	nombre_area_metrop text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.mt_sentencia (
+CREATE TABLE mt_sentencia (
 	certificado text NULL,
 	clase text NULL,
 	descargar text NULL,
 	ciudad text NULL,
-	estado text NULL,
+	estado text NULL DEFAULT 'Nuevo'::text,
 	paginas text NULL,
-	tipo_archivo text NULL
+	tipo_archivo text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_area (
+CREATE TABLE tt_area (
 	"key" text NULL,
 	area text NULL,
-	conteo int8 NULL
+	conteo int8 NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_carga_pdf (
+CREATE TABLE tt_carga_pdf (
 	"key" text NULL,
 	probability float8 NULL,
 	error_ratio float8 NULL,
-	low_quality text NULL
+	low_quality text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_cedula_catastral (
+CREATE TABLE tt_cedula_catastral (
 	cedula_catastral text NULL,
 	principal text NULL,
-	"key" text NULL
+	"key" text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_encabezado (
+CREATE TABLE tt_encabezado (
 	"key" text NULL,
 	clase text NULL,
 	descripcion text NULL,
 	fecha text NULL,
-	fecha_formato text NULL
+	fecha_formato text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_grupo_familiar (
+CREATE TABLE tt_grupo_familiar (
 	grupo text NULL,
 	n_persona text NULL,
 	"name" text NULL,
 	"key" text NULL,
-	cedula text NULL
+	cedula text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_indice_sentencia (
+CREATE TABLE tt_indice_sentencia (
 	"key" text NULL,
 	numeral text NULL,
 	texto text NULL,
 	linea int4 NULL,
-	tipo text NULL
+	tipo text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_juez_magristrado (
+CREATE TABLE tt_juez_magristrado (
 	"key" text NULL,
 	"index" int8 NULL,
 	parte text NULL,
 	original float8 NULL,
 	nombre_jm text NULL,
 	ponente int4 NULL,
-	lead_sentence text NULL
+	lead_sentence text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_localizacion (
+CREATE TABLE tt_localizacion (
 	certificado text NULL,
 	departamento text NULL,
 	codigo_departamento text NULL,
@@ -466,45 +617,57 @@ CREATE TABLE public.tt_localizacion (
 	corregimiento text NULL,
 	codigo_corregimiento text NULL,
 	vereda text NULL,
-	codigo_vereda text NULL
+	codigo_vereda text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_matricula_inmobiliaria (
+CREATE TABLE tt_log_transaccion (
+	operacion text NULL,
+	comentario text NULL DEFAULT ''::text,
+	fecha timestamp NULL DEFAULT now()
+);
+
+CREATE TABLE tt_matricula_inmobiliaria (
 	matricula_inmobiliaria text NULL,
 	principal text NULL,
-	"key" text NULL
+	"key" text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_model_predict (
+CREATE TABLE tt_model_predict (
 	accuracy float8 NULL,
 	"precision" float8 NULL,
 	sensitivity float8 NULL,
 	specificity float8 NULL,
 	f1_score float8 NULL,
 	model text NULL,
-	auc float8 NULL
+	auc float8 NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_opositor (
+CREATE TABLE tt_opositor (
 	"key" text NULL,
 	"index" int8 NULL,
 	nombre_opositor text NULL,
-	cedula text NULL
+	cedula text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_ordena_beneficios (
+CREATE TABLE tt_ordena_beneficios (
 	"key" text NULL,
 	contador int8 NULL,
-	beneficio text NULL
+	beneficio text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_ordena_entidades (
+CREATE TABLE tt_ordena_entidades (
 	"key" text NULL,
 	contador int8 NULL,
-	entidad text NULL
+	entidad text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_radicado (
+CREATE TABLE tt_radicado (
 	certificado text NULL,
 	radicacion text NULL,
 	nombre_predio text NULL,
@@ -515,10 +678,11 @@ CREATE TABLE public.tt_radicado (
 	anio text NULL,
 	cantidad_sentencias text NULL,
 	adiciona_complementa text NULL,
-	fecha_ultima_sentencia text NULL
+	fecha_ultima_sentencia text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_resuelve (
+CREATE TABLE tt_resuelve (
 	certificado text NULL,
 	resuelve text NULL,
 	radicado text NULL,
@@ -535,30 +699,61 @@ CREATE TABLE public.tt_resuelve (
 	otras_ordenes text NULL,
 	ordenes_direccion_social text NULL,
 	ordenes_catastrales text NULL,
-	usa_modelo text NULL DEFAULT 'NO'::text
+	usa_modelo text NULL DEFAULT 'NO'::text,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_resuelve_text (
+CREATE TABLE tt_resuelve_text (
 	"key" text NULL,
 	contador int8 NULL,
 	texto text NULL,
 	orderna text NULL,
 	beneficios text NULL,
-	articulos text NULL
+	articulos text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_sentencia_geo (
+CREATE TABLE tt_sentencia_geo (
 	"key" text NULL,
 	latitud text NULL,
-	longitud text NULL
+	longitud text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
 
-CREATE TABLE public.tt_solicitante (
+CREATE TABLE tt_solicitante (
 	nombre text NULL,
 	principal int8 NULL,
 	"key" text NULL,
-	cedula int8 NULL
+	cedula text NULL,
+	fecha_creacion_registro timestamp NULL DEFAULT now()
 );
+
+CREATE OR REPLACE VIEW public.vw_certificado_radicacion_finalizado
+AS SELECT ms.certificado AS "Certificado",
+    tr.radicacion AS "Radicación"
+   FROM mt_sentencia ms
+     JOIN tt_radicado tr ON tr.certificado = ms.certificado;
+
+CREATE OR REPLACE VIEW public.vw_pendiente_modelo
+AS SELECT tt_resuelve.certificado,
+    tt_resuelve.resuelve,
+    tt_resuelve.radicado,
+    tt_resuelve.orden_vivienda,
+    tt_resuelve.proyectos_productivos_beneficiarios_restitucion,
+    tt_resuelve.compensacion_victimas,
+    tt_resuelve.compensacion_terceros,
+    tt_resuelve.segundos_ocupantes,
+    tt_resuelve.alivio_predial,
+    tt_resuelve.alivio_servicios_publicos,
+    tt_resuelve.alivio_pasivos_financieros,
+    tt_resuelve.pagos_costas_gastos_judiciales,
+    tt_resuelve.administracion_proyectos_productivos_agroindustriales,
+    tt_resuelve.otras_ordenes,
+    tt_resuelve.ordenes_direccion_social,
+    tt_resuelve.ordenes_catastrales,
+    tt_resuelve.usa_modelo
+   FROM tt_resuelve
+  WHERE (tt_resuelve.orden_vivienda IS NULL OR tt_resuelve.orden_vivienda = ''::text) AND (tt_resuelve.proyectos_productivos_beneficiarios_restitucion IS NULL OR tt_resuelve.proyectos_productivos_beneficiarios_restitucion = ''::text) AND (tt_resuelve.compensacion_victimas IS NULL OR tt_resuelve.compensacion_victimas = ''::text) AND (tt_resuelve.compensacion_terceros IS NULL OR tt_resuelve.compensacion_terceros = ''::text) AND (tt_resuelve.segundos_ocupantes IS NULL OR tt_resuelve.segundos_ocupantes = ''::text) AND (tt_resuelve.alivio_predial IS NULL OR tt_resuelve.alivio_predial = ''::text) AND (tt_resuelve.alivio_servicios_publicos IS NULL OR tt_resuelve.alivio_servicios_publicos = ''::text) AND (tt_resuelve.alivio_pasivos_financieros IS NULL OR tt_resuelve.alivio_pasivos_financieros = ''::text) AND (tt_resuelve.pagos_costas_gastos_judiciales IS NULL OR tt_resuelve.pagos_costas_gastos_judiciales = ''::text) AND (tt_resuelve.administracion_proyectos_productivos_agroindustriales IS NULL OR tt_resuelve.administracion_proyectos_productivos_agroindustriales = ''::text) AND (tt_resuelve.otras_ordenes IS NULL OR tt_resuelve.otras_ordenes = ''::text) AND (tt_resuelve.ordenes_direccion_social IS NULL OR tt_resuelve.ordenes_direccion_social = ''::text) AND (tt_resuelve.ordenes_catastrales IS NULL OR tt_resuelve.ordenes_catastrales = ''::text);
 
 CREATE OR REPLACE VIEW public.vw_rol_beneficiario
 AS SELECT tgf.grupo,
@@ -601,21 +796,3 @@ AS SELECT tl.certificado,
      LEFT JOIN ( SELECT DISTINCT mt_dane_geo.codigo_cp,
             mt_dane_geo.tipo_cp
            FROM mt_dane_geo) tmp_dane_cp ON tmp_dane_cp.codigo_cp = tl.codigo_corregimiento;
-
-CREATE OR REPLACE VIEW public.vw_pendiente_modelo 
-AS SELECT * 
-FROM tt_resuelve
-WHERE 
-	(orden_vivienda is null or orden_vivienda = '') and
-	(proyectos_productivos_beneficiarios_restitucion is null or proyectos_productivos_beneficiarios_restitucion = '') and
-	(compensacion_victimas is null or compensacion_victimas = '') and
-	(compensacion_terceros is null or compensacion_terceros = '') and
-	(segundos_ocupantes is null or segundos_ocupantes = '') and
-	(alivio_predial is null or alivio_predial = '') and
-	(alivio_servicios_publicos is null or alivio_servicios_publicos = '') and
-	(alivio_pasivos_financieros is null or alivio_pasivos_financieros = '') and
-	(pagos_costas_gastos_judiciales is null or pagos_costas_gastos_judiciales = '') and
-	(administracion_proyectos_productivos_agroindustriales is null or administracion_proyectos_productivos_agroindustriales = '') and
-	(otras_ordenes is null or otras_ordenes = '') and
-	(ordenes_direccion_social is null or ordenes_direccion_social = '') and
-	(ordenes_catastrales is null or ordenes_catastrales = '')
